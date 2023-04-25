9a2434806a9ae94fd10ecfa1e81c6a2810b559bf
magajisuleiman <magajisuleiman1203@gmail.com>
2023-04-25 02:16:38 -0700

    Complete printf project and compiling perfectly

--- handle_print.c
+++ handle_print.c
@@ -15,13 +15,7 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int j;
-	int unk_len;
-	int p_chars;
-
-	unk_len = 0;
-	p_chars = -1;
-	j = 0;
+	int i = 0, unknow_len = 0, printed_chars = -1;
 
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
@@ -30,21 +24,20 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-
-	while (fmt_types[j].fmt != '\0')
+	while (fmt_types[i].fmt != '\0')
 	{
-		if (fmt[*ind] == fmt_types[j].fmt)
-			return (fmt_types[j].fn(list, buffer, flags, width, precision, size));
-	j++;
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+		i++;
 	}
 
-	if (fmt_types[j].fmt == '\0')
+	if (fmt_types[i].fmt == '\0')
 	{
-		return ((fmt[*ind] == '\0') ? (-1) : (0));
-
-		unk_len = unk_len +  write(1, "%%", 1);
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-			unk_len = unk_len +  write(1, " ", 1);
+			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
@@ -54,11 +47,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 				--(*ind);
 			return (1);
 		}
-		unk_len = unk_len +  write(1, &fmt[*ind], 1);
-		return (unk_len);
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
-	return (p_chars);
+	return (printed_chars);
 }
-
-
-
